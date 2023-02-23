ee89481ae45f75b2f04e5e4258f96f767777c8a5
ofmdggs <106493300+ofmdggs@users.noreply.github.com>
2023-02-23 19:53:00 +0300

    aa

---
+++ gitdemo/gitdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitdemoApp.swift
+//  gitdemo
+//
+//  Created by Даниил Палади on 23.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct gitdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
