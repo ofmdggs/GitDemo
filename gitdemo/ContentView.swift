a40e84035dcd336323ec681a2ce105e5df809814
ofmdggs <106493300+ofmdggs@users.noreply.github.com>
2023-02-24 01:06:25 +0300

    update text

--- gitdemo/ContentView.swift
+++ gitdemo/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Danya!")
+            Text("Hello, world!")
         }
         .padding()
     }
